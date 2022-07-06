231d86899b01145258955d7a8989c2350daf4f9b
andrey <a.serbin@litebox.ru>
2022-07-07 00:35:38 +0400

    переименовал фрагменты

--- app/src/main/java/com/example/notesvsshoppinglist/ui/dashboard/ChecklistViewModel.kt
+++ app/src/main/java/com/example/notesvsshoppinglist/ui/dashboard/ChecklistViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.notesvsshoppinglist.ui.notifications
+package com.example.notesvsshoppinglist.ui.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ChecklistViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
