1b2e04946e1f79a15c375d9f8c4dae7b49badc65
Rinat <mr.rino@gmail.com>
2022-07-24 10:21:22 +0300

    Added CalendarViewModel to Koin modules

--- app/src/main/java/com/example/notesvsshoppinglist/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/notesvsshoppinglist/ui/calendar/CalendarFragment.kt
@@ -6,26 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.notesvsshoppinglist.databinding.FragmentCalendarBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CalendarFragment : Fragment() {
 
     private var _binding: FragmentCalendarBinding? = null
     private val binding get() = _binding!!
 
+    private val calendarViewModel: CalendarViewModel by viewModel()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(CalendarViewModel::class.java)
-
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
 
         val textView: TextView = binding.textCalendar
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        calendarViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return binding.root
