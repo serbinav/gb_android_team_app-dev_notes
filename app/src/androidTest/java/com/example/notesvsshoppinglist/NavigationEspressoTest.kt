bb57855e1ba3a116c9307061522582f32ec20c83
AndreyCas91 <VfcnMGbrF11!>
2022-08-01 15:02:39 +0700

    Тест BottomNavigationView со своим матчером.

--- app/src/androidTest/java/com/example/notesvsshoppinglist/NavigationEspressoTest.kt
+++ app/src/androidTest/java/com/example/notesvsshoppinglist/NavigationEspressoTest.kt
@@ -6,12 +6,10 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.notesvsshoppinglist.EspressoUtils.hasCheckedItem
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-
 
 @RunWith(AndroidJUnit4::class)
 class NavigationEspressoTest {
@@ -25,26 +23,26 @@ class NavigationEspressoTest {
 
     @Test
     fun bottomNavigation_checklistFragmentClick() {
-        onView(withId(R.id.navigation_notes)).perform(click())
-        onView(withId(R.id.nav_view)).check(matches(hasCheckedItem(R.id.navigation_notes)))
+        onView(withId(R.id.navigation_checklist)).perform(click())
+        onView(withText("This is dashboard Fragment")).check(matches(isDisplayed()))
     }
 
     @Test
     fun bottomNavigation_notesFragmentClick() {
-        onView(withId(R.id.navigation_checklist)).perform(click())
-        onView(withId(R.id.nav_view)).check(matches(hasCheckedItem(R.id.navigation_checklist)))
+        onView(withId(R.id.navigation_notes)).perform(click())
+        onView(withText("This is home Fragment")).check(matches(isDisplayed()))
     }
 
     @Test
     fun bottomNavigation_calendarFragmentClick() {
         onView(withId(R.id.navigation_calendar)).perform(click())
-        onView(withId(R.id.nav_view)).check(matches(hasCheckedItem(R.id.navigation_calendar)))
+        onView(withText("This is notifications Fragment")).check(matches(isDisplayed()))
     }
 
     @Test
     fun bottomNavigation_settingsFragmentClick() {
         onView(withId(R.id.navigation_settings)).perform(click())
-        onView(withId(R.id.nav_view)).check(matches(hasCheckedItem(R.id.navigation_settings)))
+        onView(withText("This is Setting Fragment")).check(matches(isDisplayed()))
     }
 
     @After
@@ -52,5 +50,3 @@ class NavigationEspressoTest {
         scenario.close()
     }
 }
-
-
